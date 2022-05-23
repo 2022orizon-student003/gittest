36bfe1de4bc6a685f07df0eae0166ef0ff684090
2022orizon-student003 <106055729+2022orizon-student003@users.noreply.github.com>
2022-05-23 16:52:55 +0900

    Update gorilla.java

--- gorilla.java
+++ gorilla.java
@@ -1,4 +1,6 @@
 public class gorilla {
     public static void main(String[]args)
-    System.out.println("ゴリラが１１匹")
+    System.out.println("ゴリラが１１匹");
+    System.out.println("バナナが７本")
+        
 }
